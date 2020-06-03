14fe04cff5297d065481e89fe6cf471a1d405f29
AndreySasin0 <andreysasin00@yandex.ru>
2020-06-04 00:01:23 +0400

    Reformat: deincapsulated (mb smth went wrong, need check)

--- src/entity.py
+++ src/entity.py
@@ -12,72 +12,57 @@ class Dead(Exception):
 class Entity:
 
     def __init__(self, name, level, attack: Damage, abilities: list, health: int, resists: Resists):
-        self.__name = name
-        self.__level = level
-        self.__attack = attack
-        self.__abilities = abilities
-        self.__health_constraint = health
-        self.__health = health
-        self.__resists = resists
-        self.__active_abilities = []
-
-    def get_name(self):
-        return self.__name
-
-    def get_level(self):
-        return self.__level
-
-    def get_abilities(self):
-        return self.__abilities
-
-    def get_attack(self):
-        return self.__attack
-
-    def get_resists(self):
-        return self.__resists
+        self.name = name
+        self.level = level
+        self.attack = attack
+        self.abilities = abilities
+        self.health_constraint = health
+        self.health = health
+        self.resists = resists
+        self.active_abilities = []
 
     def add_resists(self, resists: Resists):
-        self.__resists.set_math(self.__resists.get_math() + resists.get_math())
-        self.__resists.set_phys(self.__resists.get_phys() + resists.get_phys())
-        self.__resists.set_phil(self.__resists.get_phil() + resists.get_phil())
-        self.__resists.set_prog(self.__resists.get_prog() + resists.get_prog())
+        self.resists.set_math(self.resists.get_math() + resists.get_math())
+        self.resists.set_phys(self.resists.get_phys() + resists.get_phys())
+        self.resists.set_phil(self.resists.get_phil() + resists.get_phil())
+        self.resists.set_prog(self.resists.get_prog() + resists.get_prog())
 
-    def subtract_resists(self, resists: Resists):
-        self.__resists.set_math(self.__resists.get_math() - resists.get_math())
-        self.__resists.set_phys(self.__resists.get_phys() - resists.get_phys())
-        self.__resists.set_phil(self.__resists.get_phil() - resists.get_phil())
-        self.__resists.set_prog(self.__resists.get_prog() - resists.get_prog())
+    def subtrat_resists(self, resists: Resists):
+        self.resists.set_math(self.resists.get_math() - resists.get_math())
+        self.resists.set_phys(self.resists.get_phys() - resists.get_phys())
+        self.resists.set_phil(self.resists.get_phil() - resists.get_phil())
+        self.resists.set_prog(self.resists.get_prog() - resists.get_prog())
 
     def attack(self, target):
-        return target.damage(self.__attack)
+        return target.damage(self.attack)
 
     def damage(self, damage: Damage):
-        effective_damage = int(damage.get_math() * (1 - self.__resists.get_math() if self.__resists.get_math() <= 1 else 0) +
-                             damage.get_phil() * (1 - self.__resists.get_phil() if self.__resists.get_phil() <= 1 else 0) +
-                             damage.get_phys() * (1 - self.__resists.get_phys() if self.__resists.get_phys() <= 1 else 0) +
-                             damage.get_prog() * (1 - self.__resists.get_prog() if self.__resists.get_prog() <= 1 else 0) +
-                             damage.get_abs())
-        self.__health -= effective_damage
-        if self.__health <= 0:
+        effective_damage = int(damage.get_math() * (1 - self.resists.get_math() if self.resists.get_math() <= 1 else 0) +
+                               damage.get_phys() * (1 - self.resists.get_phys() if self.resists.get_phys() <= 1 else 0) +
+                               damage.get_phil() * (1 - self.resists.get_phil() if self.resists.get_phil() <= 1 else 0) +
+                               damage.get_prog() * (1 - self.resists.get_prog() if self.resists.get_prog() <= 1 else 0) +
+                               damage.get_abs())
+        self.health -= effective_damage
+        if self.health <= 0:
             raise Dead
         return effective_damage
 
     def heal(self, amount):
-        self.__health += amount
-        if self.__health > self.__health_constraint:
-            self.__health = self.__health_constraint
+        self.health += amount
+        if self.health > self.health_constraint:
+            self.health = self.health_constraint
 
     def activate_ability(self, index: int):
-        ability = self.__abilities[index]
+        ability = self.abilities[index]
         if ability.is_passive():
-            self.__active_abilities.append(ability)
+            self.active_abilities.append(ability)
 
     def tick(self):
-        for ability in self.__abilities:
+        for ability in self.abilities:
             ability.tick()
-        for ability in self.__active_abilities:
+        for ability in self.active_abilities:
             if ability.get_duration() == 0:
-                self.__active_abilities.remove(ability)
+                self.active_abilities.remove(ability)
             else:
                 ability.use()
 
@@ -95,19 +80,20 @@ class InsufficientFunds(Exception):
 class Player(Entity):
 
     def __init__(self, name):
-        super().__init__(name, 1, Damage(10, 0, 0, 0, 0), [], 120, Resists(0.1, 0.1, 0.1, 0.1))
-        self.__head = None
-        self.__hand = None
-        self.__body = None
-        self.__items = []
-        self.__location = None
-        self.__money = 0
+        super().__init__(name, 1, Damage(10, 0, 0, 0, 0),
+                         [], 120, Resists(0.1, 0.1, 0.1, 0.1))
+        self.head = None
+        self.hand = None
+        self.body = None
+        self.items = []
+        self.location = None
+        self.money = 0
 
     # Maybe i messed up real bad here, gotta see. This one is hella complicated
     def use_item(self, item):
         # First check whether we have an index or an object copy
         if isinstance(item, int):
-            item_object = self.__items[item]
+            item_object = self.items[item]
         elif isinstance(item, Item):
             item_object = item
         else:
@@ -119,22 +105,22 @@ class Player(Entity):
         # If something is in the necessary slot, cancel its behavior first and push it back to the inventory
         # Then place the item in the necessary slot
         if slot is 'head':
-            if self.__head is not None:
-                self.__head.unuse(self)
-                self.__items.append(self.__head)
-            self.__head = item_object
+            if self.head is not None:
+                self.head.unuse(self)
+                self.items.append(self.head)
+            self.head = item_object
             print("Вы надели на голову", item_object.get_name())
         elif slot is 'hand':
-            if self.__hand is not None:
-                self.__hand.unuse(self)
-                self.__items.append(self.__hand)
-            self.__hand = item_object
+            if self.hand is not None:
+                self.hand.unuse(self)
+                self.items.append(self.hand)
+            self.hand = item_object
             print("Вы взяли в руку", item_object.get_name())
         elif slot is 'body':
-            if self.__body is not None:
-                self.__body.unuse(self)
-                self.__items.append(self.__body)
-            self.__body = item_object
+            if self.body is not None:
+                self.body.unuse(self)
+                self.items.append(self.body)
+            self.body = item_object
             print("Вы надели", item_object.get_name())
         else:
             # If it does not occupy any slots - simply use it
@@ -144,48 +130,49 @@ class Player(Entity):
             item_object.use(self)
         except Consumed:
             # If that is a consumable, it will signal that it has to be destroyed after using
-            self.__items.remove(item_object)
+            self.items.remove(item_object)
 
     def move(self, location: Location):
-        self.__location = location
+        self.location = location
 
     def deposit(self, amount):
-        if amount > self.__money:
+        if amount > self.money:
             raise InsufficientFunds
-        self.__money -= amount
+        self.money -= amount
 
     def credit(self, amount):
-        self.__money += amount
+        self.money += amount
 
     def give(self, item: Item):
-        if len(self.__items) == 10:
+        if len(self.items) == 10:
             raise InventoryFull
-        self.__items.append(item)
+        self.items.append(item)
 
     def throw(self, item):
         # First check whether we have an index or an object copy
         if isinstance(item, int):
-            item_object = self.__items[item]
+            item_object = self.items[item]
         elif isinstance(item, Item):
             item_object = item
         else:
             # If neither - something went terribly wrong
             raise ValueError
-        self.__items.remove(item_object)
+        self.items.remove(item_object)
 
     def attack(self, target):
-        rv = target.damage(self.__attack)
-        if self.__head is glasses:
+        rv = target.damage(self.attack)
+        if self.head is glasses:
             self.heal(rv / 2)
         return rv
 
     def levelup(self):
-        self.__level += 1
-        self.__health_constraint += 10
-        self.__attack().set_phys(self.__attack.get_phys() + 15)
+        self.level += 1
+        self.health_constraint += 10
+        self.attack().set_phys(self.attack.get_phys() + 15)
 
 
 # Nobody should mess with the entity pool
+# убирать тут черточки или нет хуй проссышь
 __entity_pool = [list() for i in range(4)]
 # TODO fill up with desired entities manually
 
@@ -193,4 +180,3 @@ __entity_pool = [list() for i in range(4)]
 def random_entity(level: int) -> Entity:
     constraint = len(__entity_pool[level-1])
     return __entity_pool[level-1][randint(0, constraint-1)]
-
