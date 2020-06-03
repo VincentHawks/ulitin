5d43c50cd883636c97082385f504713a189b679e
VincentHawks <qwertar3@gmail.com>
2020-06-03 15:08:47 +0300

    Refactoring: deincapsulated

--- src/location.py
+++ src/location.py
@@ -2,37 +2,17 @@ from src.entity import *
 from src.item import shitty_complex, regular_complex, godly_complex
 from random import randint
 
+
 class Location:
-    __level: int
-    __name: str
+    level: int
+    name: str
 
     def __init__(self, name, level, entity: Entity = None, exits=None):
-        self.__name = name
-        self.__exits = exits
-        self.__level = level
-        self.__entity = entity
-        self.__exits = exits
-
-    def get_level(self):
-        return self.__level
-
-    def get_name(self):
-        return self.__name
-
-    def get_exits(self):
-        return self.__exits
-
-    def get_entity(self):
-        return self.__entity
-
-    def populate_exits(self, exits: list):
-        self.__exits = exits
-
-    def insert_entity(self, entity: Entity):
-        self.__entity = entity
-
-    def kill_entity(self):
-        self.__entity = None
+        self.name = name
+        self.exits = exits
+        self.level = level
+        self.entity = entity
+        self.exits = exits
 
     def has_entity(self):
         return self.__entity is None
@@ -45,46 +25,36 @@ class Cafeteria(Location):
 
     def __init__(self):
         super().__init__("Столовая", 1)
-        self.__menu = [shitty_complex, regular_complex, godly_complex]
-        self.__choice = self.__menu[randint(0, 2)]
-        self.__cooldown_threshold = 20
-        self.__cooldown = 0
-        self.__free_complex = True
-
-    # Just to be safe
-    def insert_entity(self, entity: Entity):
-        pass
-
-    def has_entity(self):
-        return False
-
-    def kill_entity(self):
-        pass
+        self.menu = [shitty_complex, regular_complex, godly_complex]
+        self.choice = self.menu[randint(0, 2)]
+        self.cooldown_threshold = 20
+        self.cooldown = 0
+        self.free_complex = True
 
     def tick(self):
-        if self.__cooldown > 0:
-            self.__cooldown -= 1
+        if self.cooldown > 0:
+            self.cooldown -= 1
         else:
-            self.__choice = self.__menu[randint(0, 2)]
-            self.__cooldown = self.__cooldown_threshold
+            self.choice = self.menu[randint(0, 2)]
+            self.cooldown = self.cooldown_threshold
 
     def shop(self, target: Player):
         print("Добро пожаловать в кафе \"5 минут\"! Что хотели бы приобрести?")
         while True:
-            decision = input('1. ' + self.__choice.get_name() +
+            decision = input('1. ' + self.choice.name +
                              ": 100 монет.\n2. Красный компот: 20 монет. \n3. Выход.\n?: ")
             if decision is '1':
-                item = self.__choice
+                item = self.choice
             elif decision is '2':
                 item = red_cumpot
             elif decision is '3':
                 return
             try:
-                target.deposit(item.get_price())
+                target.deposit(item.price)
                 target.give(item)
             except InventoryFull:
                 print("Ваш инвентарь полон!")
-                target.credit(item.get_price())
+                target.credit(item.price)
             except InsufficientFunds:
                 print("Не хватает денег!")
 
@@ -94,16 +64,6 @@ class Basement(Location):
     def __init__(self):
         super().__init__("Подвал", 1)
         self.shop()
-
-    # Just to be safe
-    def insert_entity(self, entity: Entity):
-        pass
-
-    def has_entity(self):
-        return False
-
-    def kill_entity(self):
-        pass
 
     # TODO implement
     def shop(self):
